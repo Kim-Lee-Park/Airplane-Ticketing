add4d47695c4b24b7ed8dc3ed1688681422a479a
dnjsals45 <dnjsals45@naver.com>
2025-10-30 00:58:36 +0900

    Init Commit

---
+++ src/main/java/com/sparta/airplane/ticketing/TicketingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.airplane.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingApplication.class, args);
+    }
+
+}
